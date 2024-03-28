b1dbcdb912bf050097d8c568d59a3eed5634726a
tiagoaugusto <112525927+TiagoAugustoSilva@users.noreply.github.com>
2024-03-28 12:31:45 -0300

    Create styles.jsx

---
+++ button/styles.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+margin: 1px;
+border: 2px solid ##3300cc;
+background-color: #000000;
+color:#00000;
+font-size: 24px;
+font-weight: 700;
+flex: 1;
+border-radius: 0;
+
+&:hover {
+    opacity: 0.8;
+}
+
+`
+
+

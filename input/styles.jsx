caa4a467be737aba8dffbb77ce48d4b08405c5cc
tiagoaugusto <112525927+TiagoAugustoSilva@users.noreply.github.com>
2024-03-28 12:32:45 -0300

    Create styles.jsx

---
+++ input/styles.jsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+width: 100%;
+height: 85px;
+background-color: #66cc33;
+
+
+display: flex;
+align-items: center;
+justify-content: flex-end;
+
+font-size: 24px;
+font-family: 'Roboto', 'sans';
+
+
+input {
+    width: 100%;s
+    height: 75px;
+    background-color:#66cc33;
+    border: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    padding: 0 10px;
+    font-size: 34px;
+    font-family: 'Roboto';
+    color: #FFFFFF;
+
+}
+
+`
